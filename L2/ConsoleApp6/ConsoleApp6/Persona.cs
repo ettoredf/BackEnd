b6f8dfd36d150f6329189a74073b71a234171814
ettoredf <147718125+ettoredf@users.noreply.github.com>
2024-01-31 17:48:29 +0100

    commit

---
+++ L2/ConsoleApp6/ConsoleApp6/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Eta { get; set; }
+
+        public string GetNome () 
+        { 
+            return Nome; 
+        }
+        public string GetCognome ()
+        {
+            return Cognome;
+        }
+        public int GetEta ()
+        {
+            return Eta;
+        }
+
+        public string GetDettagli ()
+        {
+            return ($"Nome: {this.Nome},\nCognome: {this.Cognome},\nEtà: {this.Eta}");
+        }
+    }
+}
---
+++ L2/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona();
+
+            persona1.Nome = "Ettore";
+            persona1.Cognome = "Di Francesco";
+            persona1.Eta = 22;
+
+            // Console.WriteLine($"Nome: {persona1.GetNome()}");
+            // Console.WriteLine($"Cognome: {persona1.GetCognome()}");
+            // Console.WriteLine($"Età: {persona1.GetEta()}");
+            Console.WriteLine($"Dettagli:\n{persona1.GetDettagli()}");
+        }
+    }
+}
