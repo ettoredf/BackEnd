d72496f720d9f2371dbd512bdc2c5ddab0117653
ettoredf <147718125+ettoredf@users.noreply.github.com>
2024-02-01 16:47:39 +0100

    commit

---
+++ L4/Be-W1-L4/Be-W1-L4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Be_W1_L4
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ScriviMenu();
+        }
+
+        public static void ScriviMenu()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("==============OPERAZIONI===============");
+            Console.WriteLine("Scegli l'operazione da effettuare:");
+            Console.WriteLine("1.: Login");
+            Console.WriteLine("2.: Logout");
+            Console.WriteLine("3.: Verifica ora e data di login");
+
+            var scelta = Console.ReadLine();
+
+            switch (scelta)
+            {
+                case "1":
+                    Utente.Login();
+                    break;
+                case "2":
+                    Utente.Logout();
+                    break;
+                case "3":
+                    Utente.StampaDataEOraLogin();
+                    break;
+            }
+            ScriviMenu();
+        }
+    }
+}
---
+++ L4/Be-W1-L4/Be-W1-L4/Utente.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Be_W1_L4
+{
+    public static class Utente
+    {
+        public static string username;
+        public static string password;
+        public static bool IsLogged;
+        public static DateTime DataLog;
+
+        public static void Login()
+        {
+            Console.WriteLine("Inserisci username:");
+            Utente.username = Console.ReadLine();
+
+            Console.WriteLine("Inserisci password:");
+            Utente.password = Console.ReadLine();
+
+            Console.WriteLine("Conferma password:");
+            string conferma = Console.ReadLine();
+
+            if ((Utente.password == conferma) && (Utente.username != ""))
+            {
+                IsLogged = true;
+                DataLog = DateTime.Now;
+                Console.WriteLine($"Utente correttamente loggato alle ore {DataOraLog}");
+            }
+            else
+            {
+                Console.WriteLine("Non è possibile effettuare il login");
+            }
+        }
+
+        public static void Logout()
+        {
+            username = "";
+            password = "";
+            IsLogged = false;
+            Console.WriteLine("Nessun utente loggato al sistema");
+        }
+
+        public static void StampaDataEOraLogin()
+        {
+            if (Utente.IsLogged == true)
+            {
+                Console.WriteLine($"L'utente {Utente.username} ha effettuato l'accesso il {Utente.DataOraLog}");
+            }
+            else
+            {
+                Console.WriteLine("Non risultano utenti loggati a sistema");
+            }
+        }
+    }
+}
+
+    
